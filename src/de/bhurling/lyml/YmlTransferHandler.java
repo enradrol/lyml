c574c5b714fab2aba23eac7e6dba27dc968fc7b4
Bjoern Hurling <bjoern.hurling@wissenswerft.net>
2013-06-03 12:42:18 +0200

    Create android string resources from yml files

--- src/de/bhurling/lyml/YmlTransferHandler.java
+++ src/de/bhurling/lyml/YmlTransferHandler.java
@@ -2,14 +2,24 @@ package de.bhurling.lyml;
 
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+import java.util.zip.ZipOutputStream;
 
 import javax.swing.TransferHandler;
 
+import org.yaml.snakeyaml.Yaml;
+
 public class YmlTransferHandler extends TransferHandler {
+
+	private static final String OPEN_RESOURCE_TAG = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<resources>\n";
+	private static final String CLOSE_RESOURCE_TAG = "</resources>\n";
 
 	@Override
 	public boolean canImport(TransferHandler.TransferSupport info) {
@@ -47,9 +57,61 @@ public class YmlTransferHandler extends TransferHandler {
 	private void parse(String filename) throws IOException {
 		ZipFile zipFile = new ZipFile(filename);
 
+		String basename = basename(filename);
+
+		ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(
+				String.format("%s-android.zip", basename)));
+
 		for (ZipEntry entry : Collections.list(zipFile.entries())) {
-			System.out.println(entry.getName());
+			if (!entry.isDirectory()) {
+				String locale = basename(entry.getName());
+
+				String nextOutEntry = String.format("values-%s/strings.xml",
+						locale);
+				zos.putNextEntry(new ZipEntry(nextOutEntry));
+
+				parse(zipFile.getInputStream(entry), locale, zos);
+
+				zos.closeEntry();
+			}
 		}
 
+		zos.close();
+
 	}
+
+	private String basename(String filename) {
+		return filename.split("\\.(?=[^\\.]+$)")[0];
+	}
+
+	private void parse(InputStream is, String locale, ZipOutputStream zos)
+			throws IOException {
+		Yaml yaml = new Yaml();
+		HashMap<?, ?> map = (HashMap<?, ?>) yaml.load(is);
+
+		zos.write(OPEN_RESOURCE_TAG.getBytes());
+		parse("", (HashMap<?, ?>) map.get(locale), zos);
+		zos.write(CLOSE_RESOURCE_TAG.getBytes());
+	}
+
+	private void parse(String prefix, HashMap<?, ?> map, ZipOutputStream zos)
+			throws IOException {
+		Iterator<?> iter = map.keySet().iterator();
+
+		while (iter.hasNext()) {
+			String nextKey = iter.next().toString();
+			String nextPrefix = String.format("%s_%s", prefix,
+					nextKey.toString());
+			Object nextValue = map.get(nextKey);
+			if (nextValue instanceof HashMap) {
+				parse(nextPrefix, (HashMap<?, ?>) nextValue, zos);
+			} else {
+				String resourceLine = String.format(
+						"    <string name=\"%s\">%s</string>\n",
+						prefix.substring(1), nextValue);
+				zos.write(resourceLine.getBytes());
+			}
+		}
+	}
+
 }
