8677da61a1873e2f14c8c9a0ab1604e129b6301b
Bjoern Hurling <bjoern.hurling@wissenswerft.net>
2014-05-15 15:46:49 +0200

    Print usage when no project key is defined

--- src/main/java/de/bhurling/lyml/Main.java
+++ src/main/java/de/bhurling/lyml/Main.java
@@ -2,7 +2,16 @@ package de.bhurling.lyml;
 
 public class Main {
 
+    private static String USAGE =
+            "\nusage: java -jar lyml.jar <api-token> [<api-token>...]\n";
+
     public static void main(String[] args) throws Exception {
+
+        if (args.length == 0) {
+            System.out.println(USAGE);
+            System.exit(1);
+        }
+
         YmlParser parser = new YmlParser(args);
         parser.fetchFromLocaleApp();
         parser.createResources();
