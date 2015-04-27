f43004b37a0a2926f330e7539ecc2338c765297c
Arne Bodenstein <arne.bodenstein@wissenswerft.net>
2015-04-27 13:16:41 +0200

    use custom ssl keystore, because of missing COMODO root certificate in default java keystore

--- src/main/java/de/bhurling/lyml/Main.java
+++ src/main/java/de/bhurling/lyml/Main.java
@@ -11,6 +11,8 @@ public class Main {
             System.out.println(USAGE);
             System.exit(1);
         }
+        String fileName = "truststore.jks"; // java does not trust the COMODO certificate for some reason
+        System.setProperty("javax.net.ssl.trustStore", fileName); // so we use a custom keystore
 
         YmlParser parser = new YmlParser(args);
         parser.fetchFromLocaleApp();
