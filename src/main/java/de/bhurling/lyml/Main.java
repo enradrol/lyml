6b49817abda3896ba7b9ad3824e8daec69755b7c
Arne Bodenstein <arne.bodenstein@wissenswerft.net>
2015-05-08 13:59:28 +0200

    move truststore file to rescources

--- src/main/java/de/bhurling/lyml/Main.java
+++ src/main/java/de/bhurling/lyml/Main.java
@@ -1,5 +1,9 @@
 package de.bhurling.lyml;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
 public class Main {
 
     private static String USAGE =
@@ -11,8 +15,24 @@ public class Main {
             System.out.println(USAGE);
             System.exit(1);
         }
-        String fileName = "truststore.jks"; // java does not trust the COMODO certificate for some reason
-        System.setProperty("javax.net.ssl.trustStore", fileName); // so we use a custom keystore
+
+        String fileName = "/truststore.jks"; // java does not trust the COMODO certificate for some reason
+
+        InputStream in = Main.class.getResourceAsStream(fileName);
+
+        final File tempFile = File.createTempFile("tmp", "jks");
+        tempFile.deleteOnExit();
+        try (FileOutputStream out = new FileOutputStream(tempFile)) {
+            byte[] buffer = new byte[1024];
+            int len = in.read(buffer);
+            while (len != -1) {
+                out.write(buffer, 0, len);
+                len = in.read(buffer);
+            }
+        }
+
+        String path = tempFile.toString();
+        System.setProperty("javax.net.ssl.trustStore", path); // so we use a custom keystore
 
         YmlParser parser = new YmlParser(args);
         parser.fetchFromLocaleApp();
